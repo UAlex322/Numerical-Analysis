3a41acb4148be1353f7acbe651624e922490455a
UAlex322 <75722089+UAlex322@users.noreply.github.com>
2022-11-12 13:12:03 +0300

    Update computational part

--- Lab2/MVS/ODENumeric/Main.cpp
+++ Lab2/MVS/ODENumeric/Main.cpp
@@ -3,66 +3,30 @@
 #include <iostream>
 using namespace std;
 
-//namespace plt = matplotlibcpp;
+// Константы, используемые в функциях
 
-/*
-
-auto f1 = [](double x, double u) {
-	return 3*u;
-};
-
-auto f2 = [](double x, double u) {
-	return 1/(x*x*x*x + 1)*u*u + u - u*u*u*sin(10.0*x);
-};
-
-Diff_equation<Point<2>, 2> lab_system{
-	[](double x, const Point<2> &p) {
-		return p[1];
-	},
-	[](double x, const Point<2> &p) {
-		return -(p[1]*p[1] + sin(p[0]));
-	}
-};
-Point<2> iv{-1.0, 0.2};
-
-int main() {
-	//auto sol2 = euler_ivp(0.0, 1.0, f, 0.01, 2.0);
-	//auto sol = ivp_no_step_adjust<double, 1>(rk4, Diff_equation<double, 1>{f1}, 0.0, 2.0, 0.0001, 3.0, 1e-4);
-	auto sol = ivp_step_adjust<double, 1>(rk4, Diff_equation<double, 1>{f2}, 0.4, 0.0, 0.01, 0.6, 1e-6, 1e-4, 10000);
-	//auto sol = ivp_no_step_adjust<Point<2>, 2>(rk4_system2, lab_system, 0.0, iv, -0.001, 2.5, 1e-3);
-	//auto sol = ivp_step_adjust<Point<2>, 2>(rk4_system2, lab_system, 0.0, iv, 0.001, 10, 1e-3, 1e-6, 1000);
-
-	cout << "x           u\n";
-	for (int i = 0; i < sol.x.size(); ++i) {
-		cout.precision(8);
-		cout << fixed << sol.x[i] << "  ";
-		cout.precision(16);
-		cout << sol.v[i] << '\n';
-		//cout << sol.v[i][0] << "  " << sol.v[i][1] << '\n'; //<< exp(3.0*sol1.x[i]) << "  " << abs(sol1.v[i] - exp(3.0*sol1.x[i])) << '\n';
-	}
-
-	//plt::plot(sol1.x, sol1.v);
-}
-*/
-
-const double C1 = 0.1082099375634999;
-const double C2 = 1.8917900624365001;
-const double C3=  0.1427998568251809;
-const double C4 = 2.1641463834260162;
+const double C1 = -0.3393176035227834;
+const double C2 = 0.3393176035227834;
+const double C3=  -0.4920418012319509;
+const double C4 = 1.0560782612867800;
 const double C5 = M_PI/(2.0*sqrt(2.0));
 const double C6 = 8*sqrt(2)/(M_PI * M_PI);
 const double INV_3 = 1/3.0;
 
+// Функции, соответствующие участкам непрерывности истинного решения
+
 inline double u1(double x) {
-	return -1.0 + C1*exp(x) + C2*exp(-x);
+	return 1.0 + C1*exp(x) + C2*exp(-x);
 }
 
 inline double u2(double x) {
-	return -C6 + C3*exp(C5*x) + C4*exp(-C5*x);
+	return C6 + C3*exp(C5*x) + C4*exp(-C5*x);
 }
 
+// Функции, участвующие в решении основной задачи
+
 inline double k1(double a, double b) {
-	return 1/sqrt(2)*(log(tan(b/2.0 + 0.1)/tan(a/2.0 + 0.1)));
+	return M_SQRT1_2*(log(tan(b/2.0 + 0.1)/tan(a/2.0 + 0.1)));
 }
 
 inline double k2(double a, double b) {
@@ -85,7 +49,7 @@ inline double f2(double a, double b) {
 	return sin(b) - sin(a);
 }
 
-
+// Функции, участвующие в решении тестовой задачи
 
 inline double k1t(double a, double b) {
 	return b-a;
@@ -112,10 +76,9 @@ inline double f2t(double a, double b) {
 }
 
 
-
 int main() {
 	
-	const size_t n = 1000;
+	const size_t n = 20000;
 	vector<double> x(n+1), u(n+1), v, v2;
 	double xc = 0.0;
 	double h = 1.0/(double)n;
@@ -128,14 +91,30 @@ int main() {
 		u[i] = u1(xc);
 	for (size_t i = discont_pos; i <= n; ++i, xc += h)
 		u[i] = u2(xc);
+	
 	/*
+	v = solve(n, 0.0, 1.0, 1.0, 0.0, M_PI_4, k1t, k2t, q1t, q2t, f1t, f2t);
+	double max_err = 0.0;
+	for (size_t i = 0; i <= n; ++i)
+		if (abs(u[i] - v[i]) > max_err)
+			max_err = abs(u[i] - v[i]);
+	cout.precision(16);
+	cout << "Test problem\n";
+	cout << "N = " << n << "\nMaximal error: " << max_err;
+	*/
+
+	
 	v =  solve(n, 0.0, 1.0, 1.0, 0.0, M_PI_4, k1, k2, q1, q2, f1, f2);
 	v2 = solve(2*n, 0.0, 1.0, 1.0, 0.0, M_PI_4, k1, k2, q1, q2, f1, f2);
-	*/
-	v = solve(n, 0.0, 1.0, 1.0, 0.0, M_PI_4, k1t, k2t, q1t, q2t, f1t, f2t);
-	cout.precision(16);
+	double max_err = 0.0;
 	for (size_t i = 0; i <= n; ++i)
-		cout << fixed << x[i] << "  " << u[i] << "  " << v[i] << "  " << abs(u[i] - v[i]) << '\n';
+		if (abs(v[i] - v2[i*2]) > max_err)
+			max_err = abs(v[i] - v2[i*2]);
+	cout.precision(16);
+	cout << "Main problem\n";
+	cout << "N = " << n << "\nMaximal error: " << max_err;
+	//for (size_t i = 0; i <= n; ++i)
+		//cout << fixed << x[i] << "  " << u[i] << "  " << v[i] << "  " << abs(u[i] - v[i]) << '\n';
 		//cout << fixed << x[i] << "  " << v[i] << "  " << v2[2*i] << "  " << abs(v[i] - v2[2*i]) << '\n';
 
 	return 0;
--- Lab2/MVS/ODENumeric/tridiagonal.h
+++ Lab2/MVS/ODENumeric/tridiagonal.h
@@ -7,32 +7,12 @@
 #include <iostream>
 
 
-void solveMatrix(int n, double *a, double *c, double *b, double *f, double *x)
-{
-	double m;
-	for (int i = 1; i < n; i++)
-	{
-		m = a[i]/c[i-1];
-		c[i] = c[i] - m*b[i-1];
-		f[i] = f[i] - m*f[i-1];
-	}
-
-	x[n-1] = f[n-1]/c[n-1];
-
-	for (int i = n - 2; i >= 0; i--)
-	{
-		x[i]=(f[i]-b[i]*x[i+1])/c[i];
-	}
-}
-
 using func = std::function<double(double, double)>;
 std::vector<double> solve(size_t n, double a, double b, double m1, double m2, double xi, func k1, func k2, func q1, func q2, func f1, func f2) {
 	double h = (b-a)/n;                                   // шаг численного метогда
 	size_t discont_index_a  = (size_t)(xi/h) + 1;         // индекс, на котором происходит разрыв функций при движении по точкам a + h*i
 	size_t discont_index_dp = (size_t)(xi/h - 0.5) + 1;   // индекс, на котором происходит разрыв функций при движении по точкам a + h*(i + 1/2)
 	std::vector<double> ai(n+1), di(n), pi(n);            // массивы коэффициентов дл¤ построени¤ системы
-
-	// ¬џ„»—Ћ≈Ќ»≈  ќЁ‘‘»÷»≈Ќ“ќ¬ —Ћј”
 	
 	double x = a; // текуща¤ точка вида 'x + h*i'
 	for (size_t i = 1; i < discont_index_a; ++i, x += h) {
@@ -56,100 +36,27 @@ std::vector<double> solve(size_t n, double a, double b, double m1, double m2, do
 		di[i] = q2(x, x+h)/h; // вычисление коэффициента по второй функции
 		pi[i] = f2(x, x+h)/h; // вычисление коэффициента по второй функции
 	}
-	
-
-
-	/*
-	for (size_t i = 1; i < discont_index_a; ++i)
-		ai[i] = h/k1(a + (i-1)*h, a + i*h);
-	for (size_t i = discont_index_a + 1; i <= n; ++i)
-		ai[i] = h/k2(a + (i-1)*h, a + i*h);
-	for (size_t i = 1; i < discont_index_dp; ++i) {
-		di[i] = q1(a + h/2 + (i-1)*h, a + h/2 + i*h)/h;
-		pi[i] = f1(a + h/2 + (i-1)*h, a + h/2 + i*h)/h;
-	}
-	for (size_t i = discont_index_dp + 1; i < n; ++i) {
-		di[i] = q2(a + h/2 + (i-1)*h, a + h/2 + i*h)/h;
-		pi[i] = f2(a + h/2 + (i-1)*h, a + h/2 + i*h)/h;
-	}
-	ai[discont_index_a] = h/(k1(a + (discont_index_a-1)*h, xi) + k2(xi, a + discont_index_a*h));
-	di[discont_index_dp] = (q1(a + h/2 + (discont_index_dp-1)*h, xi) + q2(xi, a + h/2 + discont_index_dp*h))/h;
-	pi[discont_index_dp] = (f1(a + h/2 + (discont_index_dp-1)*h, xi) + f2(xi, a + h/2 + discont_index_dp*h))/h;
-	*/
 
 
 	double hi = 1/h;                      // обратный шаг (множитель в выражени¤х ниже)
 	std::vector<double> A(n), B(n), C(n); // коэффициенты на диагонал¤х матрицы системы
 	for (size_t i = 1; i < n; ++i) {
-		A[i] = ai[i] * hi;                             // ai[i]/(h^2)
-		B[i] = ai[i+1] * hi;                           // ai[i+1]/(h^2)
-		C[i] = (ai[i] + ai[i+1]) * hi + di[i] * h; // (ai[i] + ai[i+1])/(h^2) + di[i]
+		A[i] = ai[i] * hi;                         // ai[i]/(h^2) * h
+		B[i] = ai[i+1] * hi;                       // ai[i+1]/(h^2) * h
+		C[i] = (ai[i] + ai[i+1]) * hi + di[i] * h; // ((ai[i] + ai[i+1])/(h^2) + di[i]) * h
 		pi[i] *= h;
 	}
 
-	// ћ≈“ќƒ ѕ–ќ√ќЌ »
-
+	
 	std::vector<double> alpha(n+1), beta(n+1); // коэффициенты метода прогонки
 	std::vector<double> y(n+1);                // вектор решени¤
-
-	
 	alpha[1] = 0, beta[1] = m1;                // начальные значени¤ коэффициентов метода прогонки
 	for (size_t i = 1; i < n; ++i) {
 		double divide = 1/(C[i] - alpha[i]*A[i]); // знаменатель в коэффициентах метода прогонки
 		alpha[i+1] = B[i] * divide;
 		beta[i+1]  = (pi[i] + beta[i]*A[i]) * divide;
 	}
-	//y[0] = m1; // на основании начальных условий
 	y[n] = m2; // на основании начальных условий
-	for (size_t i = n; i >= 1; --i)
-		y[i-1] = alpha[i]*y[i] + beta[i];
-
-
-	/*
-	std::vector<double> A_(n+1), B_(n+1), C_(n+1), F_(n+1), y_(n+1);
-	for (int i = 1; i < n; ++i)
-		A_[i] = A[i];
-	A_[n] = 0.0;
-	for (int i = 1; i < n; ++i)
-		B_[i] = B[i];
-	B_[0] = 0.0;
-	for (int i = 1; i < n; ++i)
-		C_[i] = -C[i];
-	C_[0] = C_[n] = 1.0;
-	for (int i = 1; i < n; ++i)
-		F_[i] = -pi[i];
-	F_[0] = m1;
-	F_[n] = m2;
-	solveMatrix(n+1, A_.data(), C_.data(), B_.data(), F_.data(), y_.data());
-	
-
-
-	std::cout << "COMPARISON\n";
-	for (int i = 0; i <= n; ++i) {
-		std::cout << y[i] << "  " << y_[i] << '\n';
-	}
-	*/
-	return y;
-}
-
-std::vector<double> tridiagonal_solve(size_t n,
-									  const std::vector<double> &a,
-									  const std::vector<double> &b,
-									  const std::vector<double> &c,
-									  const std::vector<double> &p,
-									  double k1,
-									  double k2,
-									  double m1,
-									  double m2) {
-	std::vector<double> y(n+1), alpha(n+1), beta(n+1);
-
-	alpha[1] = k1, beta[1] = m1;
-	for (size_t i = 1; i < n; ++i) {
-		double divide = 1/(c[i] - alpha[i]*a[i]);
-		alpha[i+1] =  b[i] * divide;
-		beta[i+1]  = (p[i] + beta[i]*a[i]) * divide;
-	}
-	y[n] = (m2 + k2*beta[n])/(1 - k2*alpha[n]);
 	for (size_t i = n; i >= 1; --i)
 		y[i-1] = alpha[i]*y[i] + beta[i];
 
